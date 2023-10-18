d295df7ae91e95b13811f258d1b2b0e5968a0e21
Omi_dingus <bimb.a@mail.ru>
2023-10-18 10:39:04 +0300

    Modified squareEquaiton.cpp to include square equation check

--- squareEquation.cpp
+++ squareEquation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+
 int main() {
 	float a;
 	float b;
@@ -14,16 +15,19 @@ int main() {
 	std::cout << "Input the third coefficient number: ";
         std::cin >> c;
 	disc = (b * b) - (4 * a * c);
-	if (disc < 0) {
-		std::cout << "No real roots" << '\n';
-	} else {
-		x1 = (-b + sqrt(disc)) / 2;
-                x2 = (-b - sqrt(disc)) / 2;
-		if (abs(x1 - x2) <= 0) {
-			std::cout << "x1 = x2 = " << x1 << '\n';
+	if (abs(a - 0.0) <= 0) {
+		std::cout << "Not a square equation" << '\n';
+	} else { if (disc < 0) {
+			std::cout << "No real roots" << '\n';
 		} else {
-			std::cout << "x1 = " << x1 << '\n';
-			std::cout << "x2 = " << x2 << '\n';
+			x1 = (-b + sqrt(disc)) / 2;
+                	x2 = (-b - sqrt(disc)) / 2;
+			if (abs(x1 - x2) <= 0) {
+				std::cout << "x1 = x2 = " << x1 << '\n';
+			} else {
+				std::cout << "x1 = " << x1 << '\n';
+				std::cout << "x2 = " << x2 << '\n';
+			}
 		}
 	}
 	return(0);
